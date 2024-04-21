ff92eff376abd9e81df48d01bb47f150243ab297
Shane Witbeck <switbeck@mediafly.com>
2024-04-21 11:35:45 -0700

    fix: Remove unused configuration properties scan

--- src/main/java/dev/surly/ai/collab/SpringAICollabApplication.java
+++ src/main/java/dev/surly/ai/collab/SpringAICollabApplication.java
@@ -2,11 +2,7 @@ package dev.surly.ai.collab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@ConfigurationPropertiesScan("dev.surly.ai.collab.config")
 @SpringBootApplication
 public class SpringAICollabApplication {
 
